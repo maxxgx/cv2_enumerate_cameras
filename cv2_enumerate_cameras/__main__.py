566b66c481147b9a3a26effca7c79fc8627e7317
Arclight <chinaheyu@outlook.com>
2025-09-12 12:34:01 +0800

    refactor script

--- cv2_enumerate_cameras/__main__.py
+++ cv2_enumerate_cameras/__main__.py
@@ -1,4 +1,5 @@
 # type: ignore
+import shutil
 from cv2_enumerate_cameras import enumerate_cameras, supported_backends
 from cv2_enumerate_cameras.camera_info import CAP_ANY
 
@@ -7,44 +8,77 @@ try:
     from cv2.videoio_registry import getBackendName
 except ModuleNotFoundError:
     def getBackendName(api):
-        return str(api)
+        return {
+            0: 'CAP_ANY',
+            200: 'CAP_GSTREAMER',
+            700: 'CAP_DSHOW',
+            1200: 'CAP_AVFOUNDATION',
+            1400: 'CAP_MSMF',
+            1800: 'CAP_V4L2',
+        }[api]
+
+
+def print_table(titles, rows, title_aligns, column_aligns):
+    terminal_size = shutil.get_terminal_size()
+    column_count = len(titles)
+
+    column_widths = [max(len(rows[j][i]) for j in range(len(rows))) for i in range(column_count)]
+    column_widths = [max(len(titles[i]), column_widths[i]) for i in range(column_count)]
+
+    reserved_width = sum(column_widths[:-1]) + column_count * 3 + 1
+
+    column_widths[-1] = min(column_widths[-1], terminal_size.columns - reserved_width)
+
+    def print_row(row, aligns):
+        columns = []
+        for i in range(column_count):
+            w = column_widths[i]
+            content = row[i]
+
+            if len(content) > w:
+                content = content[:w - 3] + '...'
+
+            if aligns[i] == 'l':
+                columns.append(content.ljust(w))
+            elif aligns[i] == 'c':
+                columns.append(content.center(w))
+            elif aligns[i] == 'r':
+                columns.append(content.rjust(w))
+            else:
+                raise ValueError('Invalid alignment value')
+        print('| ' + ' | '.join(columns) + ' |')
+
+    separator = '+' + '+'.join(['-' * (w + 2) for w in column_widths]) + '+'
+
+    print(separator)
+    print_row(titles, title_aligns)
+    print(separator)
+    for row in rows:
+        print_row(row, column_aligns)
+    print(separator)
 
 
 def main():
     for backend in [CAP_ANY, *supported_backends]:
         backend_name = getBackendName(backend)
-        print(f'Enumerate using {backend_name} backend:')
+        print(f'{backend_name} backend:')
         camera_info_list = enumerate_cameras(backend)
         if not camera_info_list:
             print(f"No camera on {backend_name} backend.")
             continue
 
-        name_column_length = max(max((len(i.name) for i in camera_info_list)), 4)
-        path_column_length = max([len(i.path) for i in camera_info_list if i.path is not None] + [4])
-
-        separate_line = "+-------+-" + "-" * name_column_length + "-+------+------+-" + "-" * path_column_length + "-+"
-        output_rows = [
-            separate_line,
-            "| index | " + "name".center(name_column_length) + " | vid  | pid  | " + "path".center(path_column_length) + " |",
-            separate_line
-        ]
-        for i in camera_info_list:
-            row = f"| {i.index:5} | " + i.name.rjust(name_column_length) + " | "
-            if i.vid is None:
-                row += f" --  | "
-            else:
-                row += f"{i.vid:04X} | "
-            if i.pid is None:
-                row += f" --  | "
-            else:
-                row += f"{i.pid:04X} | "
-            if i.path:
-                row += i.path.rjust(path_column_length) + " |"
-            else:
-                row += "--".center(path_column_length) + " |"
-            output_rows.append(row)
-        output_rows.append(separate_line)
-        print('\n'.join(output_rows))
+        print_table(
+            titles=("index", "name", "vid", "pid", "path"),
+            rows=[(
+                str(camera_info.index),
+                camera_info.name,
+                " -- " if camera_info.vid is None else "{:04X}".format(camera_info.vid),
+                " -- " if camera_info.pid is None else "{:04X}".format(camera_info.pid),
+                "" if camera_info.path is None else camera_info.path,
+            ) for camera_info in camera_info_list],
+            title_aligns=('c', 'c', 'c', 'c', 'c'),
+            column_aligns=('r', 'l', 'r', 'r', 'l'),
+        )
 
 
 if __name__ == '__main__':
